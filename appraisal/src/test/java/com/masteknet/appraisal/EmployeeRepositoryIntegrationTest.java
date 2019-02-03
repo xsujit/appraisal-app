09fa7c8c54dde473b677e4816860a0a9ab50c05f
Sujit <xsujit@gmail.com>
2019-02-03 17:19:14 +0000

    Added a unit test

---
+++ appraisal/src/test/java/com/masteknet/appraisal/EmployeeRepositoryIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.masteknet.appraisal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import com.masteknet.appraisal.entities.Employee;
+import com.masteknet.appraisal.entities.Project;
+import com.masteknet.appraisal.entities.User;
+import com.masteknet.appraisal.repositories.EmployeeRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class EmployeeRepositoryIntegrationTest {
+	
+	@Autowired
+    private TestEntityManager entityManager;
+ 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    
+    @Test
+    public void whenFindByName_thenReturnEmployee() {
+        // given
+    	Project project = new Project();
+    	project.setDescription("SLC Onsite TnM");
+    	project.setTitle("SLC Onsite");
+    	project.setId(1);
+    	User user = new User("password", "sujit@mastek.com", project);
+        Employee sujit = new Employee(11226, user, "Sujit", "Mohanan", "Glasgow");
+        entityManager.persist(sujit);
+        entityManager.flush();
+     
+        // when
+        Employee found = employeeRepository.findById(11226);
+     
+        // then
+        assertThat(found.getUser())
+          .isEqualTo(sujit.getUser());
+    }
+
+}
