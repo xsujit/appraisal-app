d00cade600e011fcdf7a9bd1a71aad237f6fbd19
Sujit <xsujit@gmail.com>
2019-02-07 21:42:15 +0000

    Removed AuthenicationFacade

--- appraisal/src/main/java/com/masteknet/appraisal/auth/IAuthenticationFacade.java
+++
@@ -1,9 +0,0 @@
-package com.masteknet.appraisal.auth;
-
-import org.springframework.security.core.Authentication;
-
-public interface IAuthenticationFacade {
-
-	Authentication getAuthentication();
-	AppraisalUserPrincipal getPrincipal();
-}
